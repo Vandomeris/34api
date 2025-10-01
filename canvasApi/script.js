e2e5b8bde25738b638217ebfe4d7f405238ca4f9
Vando <vandomeri@gmail.com>
2025-10-01 13:21:54 +0300

    edited canvas game

--- canvasApi/script.js
+++ canvasApi/script.js
@@ -6,21 +6,89 @@ let x = canvas.width / 2
 let y = canvas.height - 30
 let dx = 2
 let dy = -2
+let ballRadius = 10
 
+let paddleHeight = 10
+let paddleWidth = 75
+let paddleX = (canvas.width - paddleWidth) / 2
+
+let leftPressed = false
+let rightPressed = false
+
+
+function keyDownHadler(e) {
+    if (e.key === 'ArrowLeft') {
+        leftPressed = true
+    } else if (e.key === 'ArrowRight') {
+        rightPressed = true
+    }
+}
+
+function keyUpHandler(e) {
+    if (e.key === 'ArrowLeft') {
+        leftPressed = false
+    } else if (e.key === 'ArrowRight') {
+        rightPressed = false
+    }
+}
+
+document.addEventListener('keydown', keyDownHadler)
+document.addEventListener('keyup', keyUpHandler)
+
+
+
+function drawBall() {
+    ctx.beginPath()
+    ctx.arc(x, y, ballRadius, 0, 2 * Math.PI)
+    ctx.fill()
+    ctx.closePath()
+}
+
+function drawPaddle() {
+    ctx.beginPath()
+
+    ctx.fillRect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight)
+
+    ctx.closePath()
+}
+
+
+let interval = setInterval(draw, 10)
 
 function draw() {
 
     ctx.clearRect(0, 0, canvas.width, canvas.height)
 
-    ctx.beginPath()
-    ctx.arc(x, y, 10, 0, 2 * Math.PI)
-    ctx.fill()
-    ctx.closePath()
+    drawBall()
+    drawPaddle()
+
+    if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
+        dx = -dx
+    }
+
+    if (y + dy < ballRadius) {
+        dy = -dy
+    } else if (y + dy > canvas.height - ballRadius) {
+        if (x > paddleX && x < paddleX + paddleWidth) {
+            dy = -dy
+        } else {
+            alert('Гаме Овер')
+            document.location.reload()
+            clearInterval(interval)
+        }
+
+    }
 
     x += dx
     y += dy
 
+    if (leftPressed && paddleX > 0) {
+        paddleX -= 7
+    } else if (rightPressed && paddleX < canvas.width - paddleWidth) {
+        paddleX += 7
+    }
+
+
 }
 
 
-setInterval(draw, 10)
